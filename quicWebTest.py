afa1d4166cab3d86356dc4736c5d8833e38fcc4e
Gzm <gongzhenmu@users.noreply.github.com>
2022-12-06 23:31:53 -0500

    Update quicWebTest.py

--- quicWebTest.py
+++ quicWebTest.py
@@ -1,7 +1,8 @@
 import os
 import subprocess
 
-calculateAverage = int(os.sys.argv[1]) if len(os.sys.argv) > 1 else 0
+iterations = int(os.sys.argv[1]) if len(os.sys.argv) > 1 else 5
+calculateAverage = int(os.sys.argv[2]) if len(os.sys.argv) > 2 else 0
 command = './quicWebTestOne.sh '
 
 videoFiles = []
@@ -11,11 +12,11 @@ subfolders = [ f.path for f in os.scandir(target_dir) if f.is_dir() ]
 for i in subfolders:
     folderName = i.split("/")[-1]
     finalCommand = command + folderName
-    for i in range(0, 5):
+    for i in range(iterations):
         subprocess.run(finalCommand, shell=True)
         #print(finalCommand)
     if calculateAverage == 2:
         subprocess.run("python3 averageTime.py Webtimestamp.log >> quicWebResult.log", shell=True)
 # Calculate average time 
 if calculateAverage == 1:
-    subprocess.run("python3 averageTime.py Webtimestamp.log > quicWebResult.log", shell=True)
+    subprocess.run("python3 averageTimeRaw.py Webtimestamp.log > quicWebResult.log", shell=True)
